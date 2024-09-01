61037cfbae3d1b6e8b7c78a5b3b1c4842409f7bd
NellitG <kamaunelson157@gmail.com>
2024-09-01 18:28:42 +0300

    message

--- app/src/components/AboutUsFaq.vue
+++ app/src/components/AboutUsFaq.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="py-20 bg-gray-100">
+  <section class="py-20">
     <div class="max-w-7xl mx-auto px-4 sm:px-8">
       <div class="py-8 px-4 mx-auto max-w-7xl text-center lg:py-8 lg:px-0">
         <h1
@@ -8,9 +8,9 @@
           Frequently Asked Questions
         </h1>
         <p
-          class="mb-0 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48"
+          class="mb-0 text-lg font-normal text-gray-950 lg:text-xl sm:px-16 xl:px-48"
         >
-          Exploring Tech: Answers to Your Frequently Asked Questions
+          Good Education, Good Character
         </p>
       </div>
       <div class="max-w-3xl mx-auto">
@@ -27,7 +27,7 @@
                 <span class="text-2xl">{{ faq.open ? "-" : "+" }}</span>
               </summary>
 
-              <div v-show="faq.open" class="m-4 p-4 rounded-2xl text-gray-500">
+              <div v-show="faq.open" class="m-4 p-4 rounded-2xl text-gray-950">
                 <p>
                   {{ faq.answer }}
                 </p>
@@ -45,21 +45,27 @@ import { ref } from "vue";
 
 const faqs = ref([
   {
-    question: "What is the campus culture like?",
+    question: " What age groups does the school cater to?",
     answer:
-      "Campus culture can vary greatly. Some colleges have a strong sense of community, with lots of school spirit and traditions, while others may be more diverse and cosmopolitan. It’s a good idea to visit the campus or talk to current students to get a feel for the atmosphere.",
+      "Cape Chestnut School offers educational programs for children from kindergarten through elementary grades. We are continuously expanding to accommodate different age groups and meet the needs of our community.",
     open: false,
   },
   {
-    question: "How safe is the campus?",
+    question: "What extracurricular activities are available for students?",
     answer:
-      "Campus safety is a top priority for colleges. Many have campus security, emergency alert systems, and policies to ensure student safety. It’s helpful to review the college’s annual security report or ask about safety measures.",
+      "We offer a variety of extracurricular activities, including art, music, sports, gardening, and a STEM program that encourages exploration and innovation in science, technology, engineering, and math.",
     open: false,
   },
   {
-    question: "What support services are available for students?",
+    question: "What is the student-to-teacher ratio at Cape Chestnut School?",
     answer:
-      "Support services may include academic advising, tutoring, counseling services, disability services, and health services. These resources are designed to help students succeed academically and personally.",
+      "We maintain a low student-to-teacher ratio to ensure personalized attention and support for each child. On average, our classrooms have a ratio of 10 students per teacher.",
+    open: false,
+  },
+   {
+    question: " Are meals provided at the school?",
+    answer:
+      "Yes, we provide healthy, balanced meals for all students. Our menu includes fresh produce from our school garden, emphasizing nutritious options that support our students' overall well-being.",
     open: false,
   },
 ]);
