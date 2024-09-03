3f6704cae4e34775f31fa652bd71e890092feb6c
NellitG <kamaunelson157@gmail.com>
2024-09-03 15:29:00 +0300

    message

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -133,14 +133,14 @@
                 >About Us</router-link
               >
             </li>
-            <li>
+            <!-- <li>
               <router-link
                 to="/news"
                 class="hover:text-green-500"
                 @click.native="closeMenu"
                 >News & updates</router-link
               >
-            </li>
+            </li> -->
             <li>
               <router-link
                 to="/contact"
